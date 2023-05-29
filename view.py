e9c8d3e637c9e4b07c3090761a8c20b93b39f687
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-05-29 17:14:52 +0300

    release

---
+++ view.py
@@ -0,0 +1,35 @@
+def menu():
+    print(
+        '1 - Отобразить все контакты \n'
+        '2 - Добавить контакт \n'
+        '3 - Найти контакт \n'
+        '4 - Изменить контакт \n'
+        '5 - Удалить контакт \n'
+        "6 - Выход \n"
+    )
+def error():
+    print ("Ошибка ввода ")
+
+def show_contacts(date):
+    path = 'file.txt'
+    data = open(path, 'r')
+    print(data.readlines())
+    data.close()
+
+def welcome():
+    print("Привествуем вас на желтых страницах")
+
+
+def success(res):
+    print ("Сохранено ")
+
+def fail(res):
+    print ("Ошибка ")
+
+    
+
+if __name__ == '__main__':
+    menu()
+
+
+
